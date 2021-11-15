2b8eb657ab08f22df3e85598f36a590977f15695
Maciek Brzakala <mikrut617@gmail.com>
2021-11-15 10:03:45 -0500

    updated file with bank account and user class

--- fundamentals/function_assignments/users_w_bank_accounts.py
+++ fundamentals/function_assignments/users_w_bank_accounts.py
@@ -1,8 +1,57 @@
-class User:
-    def __init__(self, name, email):
+## ABSTRACTION ##
+
+class User:		
+
+    def __init__(self, name):
         self.name = name
-        self.email = email
-        self.account = BankAccount(int_rate=0.02, balance=0)
+        self.account = {
+            "checking" : BankAccount(.00001,1000),
+            "savings" : BankAccount(.021,3000)
+        }
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, Checking Balance: {self.account['checking'].display_account_info()}")
+        print(f"User: {self.name}, Savings Balance: {self.account['savings'].display_account_info()}")
 
 
+    def transfer_money(self, amount, user):
+        self.amount -= amount
+        user.amount += amount
+        self.display_user_balance()
+        user.display_user_balance()
+
+
+class BankAccount:
+    accounts = []
+
+    def __init__(self, interest_rate, balance):
+        self.interest_rate = interest_rate
+        self.balance = balance
+        BankAccount.accounts.append(self) 
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
     
+    def display_account_info(self):
+        return f"{self.balance}"
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.interest_rate)
+        return self
+
+
+maciek = User("Maciek")     
+
+maciek.account['checking'].deposit(2400)
+maciek.account['savings'].withdraw(500)
+maciek.display_user_balance()
