cd33256cb91f86a85b1b95cdd8b85a78d9e2de1d
Maciek Brzakala <mikrut617@gmail.com>
2021-11-19 10:29:55 -0500

    create file for dojo pets assignment

---
+++ fundamentals/function_assignments/dojo_pets.py
@@ -0,0 +1,27 @@
+from types import TracebackType
+
+
+class Ninja:
+    # implement __init__( first_name , last_name , treats , pet_food , pet )
+    def __init__(self, irst_name , last_name , treats , pet_food , pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.treats = treats
+        self.pet_food = pet_food
+        self.pet = pet
+
+    # implement the following methods:
+    # walk() - walks the ninja's pet invoking the pet play() method
+    # feed() - feeds the ninja's pet invoking the pet eat() method
+    # bathe() - cleans the ninja's pet invoking the pet noise() method  
+    def walk(self):
+        pass
+
+    def feed(self):
+        pass
+
+    def bathe(self):
+        pass
+
+
+
