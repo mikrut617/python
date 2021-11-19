a9281ed8db42c6c80c2c9d3b9338a2d8ad53a1fb
Maciek Brzakala <mikrut617@gmail.com>
2021-11-19 10:35:54 -0500

    add skeleton for methods

--- fundamentals/function_assignments/dojo_pets.py
+++ fundamentals/function_assignments/dojo_pets.py
@@ -1,8 +1,6 @@
-
-
 class Ninja:
     # implement __init__( first_name , last_name , treats , pet_food , pet )
-    def __init__(self, irst_name , last_name , treats , pet_food , pet):
+    def __init__(self, first_name , last_name , treats , pet_food , pet):
         self.first_name = first_name
         self.last_name = last_name
         self.treats = treats
@@ -22,5 +20,32 @@ class Ninja:
     def bathe(self):
         pass
 
+class Pet:
+    # implement __init__( name , type , tricks ):
+    def __init__(self, name, type, tricks):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+
+    # implement the following methods:
+    # sleep() - increases the pets energy by 25
+    def sleep(self):
+        pass
+
+    # eat() - increases the pet's energy by 5 & health by 10
+    def eat(self):
+        pass
+
+    # play() - increases the pet's health by 5
+
+    def play(self):
+        pass
+
+    # noise() - prints out the pet's sound
+    def noise(self):
+        print("Meow")
 
 
+
+maciek = Ninja("Maciek") 
+marcus = Pet("Marcus")
