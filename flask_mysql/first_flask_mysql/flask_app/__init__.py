4a8c8a383cbe7ed123369d154255ecdf3e73361f
Maciek Brzakala <mikrut617@gmail.com>
2022-01-22 15:09:29 -0500

    uupdate files

---
+++ flask_mysql/first_flask_mysql/flask_app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+app = Flask(__name__)
+
+app.secret_key = "Py is Life" 
+# secret key is for sessions
+
---
+++ flask_mysql/users_crud_mod/users_crud_mod/server.py
@@ -0,0 +1,3 @@
+from flask_app import Flask
+from flask_app.controllers import users
+
