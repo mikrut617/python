e96c3bc9daec338f95fe4a0456266ae4c7fbe3fb
Maciek Brzakala <mikrut617@gmail.com>
2022-01-29 12:12:09 -0500

    update files with new db table dojos

--- flask_mysql/dojo_survey_w_validation/server.py
+++ flask_mysql/dojo_survey_w_validation/server.py
@@ -1,25 +1,5 @@
-from flask import Flask, render_template, session, redirect,request
+from flask_app import app
+from flask_app.controllers import surveys
 
-app = Flask(__name__)
-
-app.secret_key="The password is password."
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-
-@app.route('/process',methods=['POST'])
-def process():
-    session['name'] = request.form['name']
-    session['location'] = request.form['location']
-    session['language'] = request.form['language']
-    session['comments'] = request.form['comments']
-    return redirect('/success')
-
-@app.route('/success')
-def success():
-    return render_template('success.html')
-    
 if __name__=="__main__":
     app.run(debug=True)
